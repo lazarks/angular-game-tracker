efee395cd8125921c8761db6c800784e9bf56c94
lazarks <sljivic.lazar037@gmail.com>
2021-12-15 21:22:37 +0100

    recepies component setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../app/components/home/home.component';
+import { RecepiesComponent } from './components/recepies/recepies.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'recepies', component: RecepiesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { RecepiesComponent } from './components/recepies/recepies.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NavbarComponent],
+  declarations: [AppComponent, HomeComponent, NavbarComponent, RecepiesComponent],
   imports: [BrowserModule, AppRoutingModule, SlickCarouselModule],
   providers: [],
   bootstrap: [AppComponent],
