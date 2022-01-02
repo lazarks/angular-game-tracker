b74619a04df0c0eacbb98f469d9acc84d0420357
lazarks <sljivic.lazar037@gmail.com>
2022-01-02 14:16:21 +0100

    home styling finished

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Game } from 'src/app/models/game.model';
-import { HttpService } from 'src/app/services/http.service';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +7,34 @@ import { HttpService } from 'src/app/services/http.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  contactForm!: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(private el: ElementRef) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.contactForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      message: new FormControl('', [Validators.required]),
+    });
+  }
+
+  sendMessage(): void {
+    for (let key of Object.keys(this.contactForm.controls)) {
+      if (this.contactForm.controls[key].invalid) {
+        let invalidControl = this.el.nativeElement.querySelector(
+          '[formcontrolname="' + key + '"]'
+        );
+        invalidControl.focus();
+        break;
+      } else {
+        console.log(this.contactForm.controls[key].value);
+        this.contactForm.reset();
+      }
+    }
+  }
 }
